4309c7e72873eb146a0c45b8f51c09049a754899
saniu <saniulnaim@gmail.com>
2026-05-02 13:48:33 +0600

    Add AutoMapper profiles for Product DTO/entity mapping
    Added three AutoMapper profiles in BusinessLogicLayer.Mappers: ProductAddRequestToProduct, ProductToProductResponse, and ProductUpdateRequestToProduct. These profiles define property mappings between Product-related DTOs and the Product entity. Note: There is a typo in ProductUpdateRequestToProductMappingProfile ("ProductUpdateequest" should be "ProductUpdateRequest").

---
+++ BusinessLogicLayer/Mappers/ProductAddRequestToProductMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.DTO;
+using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Mappers
+{
+    public class ProductAddRequestToProductMappingProfile : Profile
+    {
+        public ProductAddRequestToProductMappingProfile()
+        {
+            CreateMap<ProductAddRequest, Product>()
+                .ForMember(dest => dest.ProductID, opt => opt.Ignore())
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.ProductName))
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
+                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice))
+                .ForMember(dest => dest.QuantityInStock, opt => opt.MapFrom(src => src.QuantityInStock));
+        }
+    }
+}
---
+++ BusinessLogicLayer/Mappers/ProductUpdateRequestToProductMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.DTO;
+using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Mappers
+{
+    public class ProductUpdateRequestToProductMappingProfile : Profile
+    {
+        public ProductUpdateRequestToProductMappingProfile()
+        {
+            CreateMap<ProductUpdateequest, Product>()
+                .ForMember(dest => dest.ProductID, opt => opt.MapFrom(src => src.ProductID))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.ProductName))
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
+                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice))
+                .ForMember(dest => dest.QuantityInStock, opt => opt.MapFrom(src => src.QuantityInStock));
+        }
+    }
+}
